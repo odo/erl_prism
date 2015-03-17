751215052cd2ce1aab08bb2d2d90a815fc80ac40
odo <florian.odronitz@wooga.net>
2015-03-17 01:45:40 +0100

    relative reductions

--- src/eappstat.erl
+++ src/eappstat.erl
@@ -10,6 +10,10 @@
 -define(GREEN, 3).
 -define(BLUE, 4).
 -define(YELLOW, 5).
+-define(RED_PALE, 6).
+-define(YELLOW_PALE, 7).
+-define(BLUE_PALE, 8).
+-define(DARKGRAY, 9).
 
 -define(WHITE_TYPE, 101).
 -define(GREEN_TYPE, 102).
@@ -21,8 +25,16 @@
 -define(RED_HL_TYPE, 108).
 -define(BLUE_TYPE, 109).
 -define(BLUE_HL_TYPE, 110).
--define(CURSORCOLOR, 111).
--define(CURSOR_HL, 111).
+
+-define(RED_PALE_TYPE, 111).
+-define(YELLOW_PALE_TYPE, 112).
+-define(BLUE_PALE_TYPE, 113).
+
+
+-define(CURSORCOLOR, 114).
+-define(CURSOR_HL, 115).
+
+-define(POOL_TYPE, 116).
 
 -define(HEADERHEIGHT, 4).
 -define(FOOTERHEIGHT, 4).
@@ -85,16 +97,27 @@ setup() ->
 
     cecho:init_color(?BLACK, 0, 0, 0),
     cecho:init_color(?WHITE, 800, 800, 800),
-    cecho:init_color(?RED, 1000, 300, 50),
     cecho:init_color(?GREEN, 100, 1000, 100),
-    cecho:init_color(?BLUE, 300, 300, 800),
+    cecho:init_color(?DARKGRAY, 50, 150, 50),
+
+    cecho:init_color(?RED, 1000, 300, 50),
     cecho:init_color(?YELLOW, 800, 800, 50),
+    cecho:init_color(?BLUE, 300, 300, 800),
+    PaleFactor = 1/3,
+    cecho:init_color(?RED_PALE,    trunc(1000 * PaleFactor), trunc(300 * PaleFactor), trunc(50 * PaleFactor)),
+    cecho:init_color(?YELLOW_PALE, trunc(800 * PaleFactor), trunc(800 * PaleFactor), trunc(50 * PaleFactor)),
+    cecho:init_color(?BLUE_PALE,   trunc(300 * PaleFactor), trunc(300 * PaleFactor), trunc(800 * PaleFactor)),
+
 
     cecho:curs_set(?ceCURS_INVISIBLE),
     cecho:init_pair(?WHITE_TYPE, ?WHITE, ?BLACK),
+    cecho:init_pair(?POOL_TYPE, ?WHITE, ?DARKGRAY),
+    cecho:init_pair(?RED_TYPE, ?RED, ?BLACK),
     cecho:init_pair(?YELLOW_TYPE, ?YELLOW, ?BLACK),
     cecho:init_pair(?BLUE_TYPE, ?BLUE, ?BLACK),
-    cecho:init_pair(?RED_TYPE, ?RED, ?BLACK),
+    cecho:init_pair(?RED_PALE_TYPE, ?RED_PALE, ?BLACK),
+    cecho:init_pair(?YELLOW_PALE_TYPE, ?YELLOW_PALE, ?BLACK),
+    cecho:init_pair(?BLUE_PALE_TYPE, ?BLUE_PALE, ?BLACK),
     cecho:init_pair(?CURSOR_HL, ?BLACK, ?CURSORCOLOR),
     {YMax, XMax} = cecho:getmaxyx(),
     Header       = cecho:newwin(?HEADERHEIGHT, XMax, 0, 0),
@@ -124,6 +147,7 @@ plot(Capture = #capture{ tree = Tree }, Env) ->
     EnvPlot.
 
 plot_header(#capture{ tree = Tree, time = Time }, #env{ header = Header, node_stats = #node_stats{ total_reductions = TotalReductions } }) ->
+    color(Header, ?WHITE_TYPE),
     {{Y, M, D}, {Hr, Min, Sec}} = calendar:now_to_universal_time(Time),
     f(1, 1, "~s@~p-~p-~pT~p:~p:~p", [Tree#node.name, Y, M, D, Hr, Min, Sec], Header),
     f(1, 2, "total reductions/s: ~p", [TotalReductions], Header),
@@ -133,15 +157,17 @@ plot_header(#capture{ tree = Tree, time = Time }, #env{ header = Header, node_st
     cecho:wrefresh(Header).
 
 plot_body(Tree, Env = #env{ body = Body } ) ->
-    Result = plot_table(Tree, Env#env{ x = 1, y = 1 }),
+    Result = plot_table(undefined, Tree, Env#env{ x = 1, y = 1 }),
     cecho:wrefresh(Body),
     Result.
 
 plot_footer(#env{ marked_node = undefined }) ->
     noop;
 plot_footer(#env{ footer = Footer, marked_node = Node = #node{ proc_info = undefined } }) ->
+    color(Footer, ?WHITE_TYPE),
     f(1, 1, "~p", [Node#node.name], Footer);
 plot_footer(#env{ footer = Footer, marked_node = Node = #node{ proc_info = ProcInfo } }) ->
+    color(Footer, ?WHITE_TYPE),
     Pid               = proplists:get_value(pid, ProcInfo),
     {Mod, Fun, Arity} = proplists:get_value(current_function, ProcInfo),
     Status            = proplists:get_value(status, ProcInfo),
@@ -163,13 +189,13 @@ total_reductions(#node{ proc_info = ProcInfo, children = Children }) ->
     end,
     Increment + lists:sum([total_reductions(Child) || Child <- Children]).
 
-plot_table(Node = #node{ type = supervisor }, Env = #env{ y = Y, body_height = BodyHeight, shift_y = ShiftY }) ->
+plot_table(Parent, Node = #node{ type = supervisor }, Env = #env{ y = Y, body_height = BodyHeight, shift_y = ShiftY }) ->
     case Y >= (BodyHeight + ShiftY) of
         true ->
             Env;
         false ->
             EnvCurrentSupPid = Env#env{ current_sup_pid = proplists:get_value(pid, Node#node.proc_info) },
-            with_color(fun() -> print(Node, EnvCurrentSupPid) end, EnvCurrentSupPid),
+            with_color(fun() -> print(Parent, Node, EnvCurrentSupPid) end, EnvCurrentSupPid),
             MarkedEnv = maybe_mark(Node, EnvCurrentSupPid),
             NewEnv = MarkedEnv#env{ y = Y + 1, x = EnvCurrentSupPid#env.x + ?INDENT },
             case is_pool(Node#node.children, NewEnv) of
@@ -178,21 +204,21 @@ plot_table(Node = #node{ type = supervisor }, Env = #env{ y = Y, body_height = B
                     PoolEnv#env{ x = NewEnv#env.x - ?INDENT };
                 false ->
                     Children = sort_by_reductions(Node#node.children),
-                    ChildEnv = lists:foldl(fun(Child, FoldEnv) -> plot_table(Child, maybe_open(FoldEnv)) end, NewEnv, Children),
+                    ChildEnv = lists:foldl(fun(Child, FoldEnv) -> plot_table(Node, Child, maybe_open(FoldEnv)) end, NewEnv, Children),
                     ChildEnv#env{ x = NewEnv#env.x - ?INDENT }
             end
     end;
 
 
-plot_table(Node, Env = #env{ y = Y, body_height = BodyHeight, shift_y = ShiftY }) ->
+plot_table(Parent, Node, Env = #env{ y = Y, body_height = BodyHeight, shift_y = ShiftY }) ->
     MarkedEnv = maybe_mark(Node, Env),
     case Y >= (BodyHeight + ShiftY) of
         true ->
             MarkedEnv;
         false ->
-            with_color(fun() -> print(Node, MarkedEnv) end, MarkedEnv),
+            with_color(fun() -> print(Parent, Node, MarkedEnv) end, MarkedEnv),
             NewEnv =  MarkedEnv#env{ y = Y + 1, x = MarkedEnv#env.x + ?INDENT },
-            ChildEnv = lists:foldl(fun(Child, FoldEnv) -> maybe_open(plot_table(Child, FoldEnv)) end, NewEnv, Node#node.children),
+            ChildEnv = lists:foldl(fun(Child, FoldEnv) -> maybe_open(plot_table(Node, Child, FoldEnv)) end, NewEnv, Node#node.children),
             ChildEnv#env{ x = NewEnv#env.x - ?INDENT}
     end.
 
@@ -203,12 +229,12 @@ plot_pool(Members, Env) ->
     Balance    = balance(Reductions),
     case is_number(Balance) of
         true ->
-            with_color(
+            with_pool_color(
                 fun() -> f(Env#env.x, Env#env.y - Env#env.shift_y, "p: procs: ~p balance: ~.1f %", [length(Members), Balance * 100], Env#env.body) end,
                 Env
              );
         false ->
-            with_color(
+            with_pool_color(
                 fun() -> f(Env#env.x, Env#env.y - Env#env.shift_y, "p: procs: ~p balance: ~s", [length(Members), Balance], Env#env.body) end,
                 Env
             )
@@ -243,22 +269,29 @@ maybe_mark(_, Env) ->
 
 with_color(Fun, #env{ cursor_y = CursorY, y = CursorY, body = Body }) ->
     color(Body, ?CURSOR_HL),
-    Fun(),
-    color(Body, ?WHITE_TYPE);
-with_color(Fun, _) ->
+    Fun();
+with_color(Fun, #env{ body = Body }) ->
+    color(Body, ?WHITE_TYPE),
     Fun().
 
-print(Node = #node{ type = node }, Env) ->
-    f(Env#env.x, Env#env.y - Env#env.shift_y, "n: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, Env#env.node_stats#node_stats.total_reductions}, Env#env.body);
-print(Node = #node{ type = application }, Env) ->
-    f(Env#env.x, Env#env.y - Env#env.shift_y, "a: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node)}, Env#env.body);
-print(Node = #node{ type = supervisor }, Env) ->
-    f(Env#env.x, Env#env.y - Env#env.shift_y, "s: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node)}, Env#env.body);
-print(Node = #node{ type = worker }, Env) ->
-    f(Env#env.x, Env#env.y - Env#env.shift_y, "w: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node)}, Env#env.body);
-print(Node = #node{ type = process }, Env) ->
-    f(Env#env.x, Env#env.y - Env#env.shift_y, "p: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node)}, Env#env.body);
-print(#node{ type = Type }, _) ->
+with_pool_color(Fun, #env{ cursor_y = CursorY, y = CursorY, body = Body }) ->
+    color(Body, ?CURSOR_HL),
+    Fun();
+with_pool_color(Fun, #env{ body = Body }) ->
+    color(Body, ?POOL_TYPE),
+    Fun().
+
+print(_, Node = #node{ type = node }, Env) ->
+    f(Env#env.x, Env#env.y - Env#env.shift_y, "n: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, Env#env.node_stats#node_stats.total_reductions, Env#env.node_stats#node_stats.total_reductions}, Env#env.body);
+print(Parent, Node = #node{ type = application }, Env) ->
+    f(Env#env.x, Env#env.y - Env#env.shift_y, "a: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node), total_reductions(Parent)}, Env#env.body);
+print(Parent, Node = #node{ type = supervisor }, Env) ->
+    f(Env#env.x, Env#env.y - Env#env.shift_y, "s: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node), total_reductions(Parent)}, Env#env.body);
+print(Parent, Node = #node{ type = worker }, Env) ->
+    f(Env#env.x, Env#env.y - Env#env.shift_y, "w: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node), total_reductions(Parent)}, Env#env.body);
+print(Parent, Node = #node{ type = process }, Env) ->
+    f(Env#env.x, Env#env.y - Env#env.shift_y, "p: ~s ", [Node#node.name], {Env#env.node_stats#node_stats.total_reductions, total_reductions(Node), total_reductions(Parent)}, Env#env.body);
+print(_, #node{ type = Type }, _) ->
     lager:info("unkonwn type: ~p\n", [Type]).
 
 is_pool([_], _) ->
@@ -294,18 +327,38 @@ f(X, Y, String, Args, Window) ->
 
 f(_, Y, _, _, _, _) when Y < 1 ->
     noop;
-f(X, Y, String, Args, {AppReds, ProcReds}, Window) ->
+f(X, Y, String, Args, {AppReds, ProcReds, ParentReds}, Window) ->
+    {_, XMax}   = cecho:getmaxyx(Window),
     Left  = fnorm(String, Args),
-    Fract = ProcReds / AppReds,
-    {_, XMax} = cecho:getmaxyx(Window),
-    Bar   = trunc(Fract * (XMax - 57)),
-    Right = ["|" || _ <- lists:seq(1, Bar)] ++ io_lib:format("~.1f%", [Fract * 100]),
+    FractTotal  = ProcReds / AppReds,
+    {RightParent, FractParent} =
+    case ParentReds of
+        0 ->
+            {undefined, undefined};
+        _ ->
+            FractP = ProcReds / ParentReds,
+            BarP   = trunc(FractP * (XMax - 57)),
+            {
+                ["|" || _ <- lists:seq(1, BarP)] ++ io_lib:format("~.1f%", [FractP * 100]),
+                FractP
+            }
+    end,
+    BarTotal    = trunc(FractTotal  * (XMax - 57)),
+    RightTotal = ["|" || _ <- lists:seq(1, BarTotal)] ++ io_lib:format("~.1f%", [FractTotal * 100]),
     case move_if_ok(Y, X, Window) of
         ok ->
             cecho:waddstr(Window, Left),
+            case RightParent of
+                undefined ->
+                    noop;
+                _ ->
+                    load_color_pale(FractParent, Window),
+                    cecho:wmove(Window, Y, 50),
+                    cecho:waddstr(Window, RightParent)
+            end,
+            load_color(FractTotal, Window),
             cecho:wmove(Window, Y, 50),
-            load_color(Fract, Window),
-            cecho:waddstr(Window, Right),
+            cecho:waddstr(Window, RightTotal),
             color(Window, ?WHITE_TYPE);
         not_ok ->
             noop
@@ -336,9 +389,17 @@ color(Window, Color, true) ->
 load_color(Fract, Window) ->
     Color =
     case Fract of
-        _ when Fract < 0.333 -> ?BLUE_TYPE;
-        _ when Fract < 0.666 -> ?YELLOW_TYPE;
+        _ when Fract < 0.25 -> ?BLUE_TYPE;
+        _ when Fract < 0.50 -> ?YELLOW_TYPE;
         _ -> ?RED_TYPE
     end,
     color(Window, Color).
 
+load_color_pale(Fract, Window) ->
+    Color =
+    case Fract of
+        _ when Fract < 0.25 -> ?BLUE_PALE_TYPE;
+        _ when Fract < 0.50 -> ?YELLOW_PALE_TYPE;
+        _ -> ?RED_PALE_TYPE
+    end,
+    color(Window, Color).
